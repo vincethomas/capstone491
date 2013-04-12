f321f4de98eb4f426e6e6709eb1b53e59c8792ce
vincet <vince.h.thomas@gmail.com>
2013-04-12 12:19:15 -0700

    working on applying facutal driver

--- src/com/example/allergyapp/MainActivity.java
+++ src/com/example/allergyapp/MainActivity.java
@@ -1,18 +1,26 @@
 package com.example.allergyapp;
 
+import java.util.List;
+import java.util.Map;
+
 import android.app.Activity;
+import android.app.DownloadManager.Query;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 
 import com.factual.driver.Factual;
+import com.factual.driver.ReadResponse;
 //import android.widget.Button;
 
 public class MainActivity extends Activity {
 	
-	private Factual factual = new Factual("","");
+	private final String OAUTH_KEY = "oQGYt52dXjhcfGJpq3QxufgRtNcNps0Kfa3DrpSk";
+	private final String OAUTH_SECRET = "4beHgBGuELmqEed765JVNGXMnOSZI6DdfFRRaSn1";
+	private Factual factual = new Factual(OAUTH_KEY,OAUTH_SECRET);
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +29,7 @@ public class MainActivity extends Activity {
 		//Button scanButton = (Button) findViewById(R.id.scanButton);
 		//scanButton.setOnClickListener(scanDatShit);
 		//IntentIntegrator.initiateScan(this);
+		FactualRetrievalTask frt = new FactualRetrievalTask();
 		Log.w("myApp", "no network");
 	}
 
@@ -58,5 +67,37 @@ public class MainActivity extends Activity {
 	        }
 	    }
 	}
+	
+	protected class FactualRetrievalTask extends AsyncTask<Query, Integer, List<ReadResponse>> {
+		@Override
+		protected List<ReadResponse> doInBackground(Query... params) {
+			List<ReadResponse> results = Lists.newArrayList();
+			for (Query q : params) {
+				results.add(factual.fetch("restaurants-us", q));
+			}
+			return results;
+		}
+
+		@Override
+		protected void onProgressUpdate(Integer... progress) {
+		}
+
+		@Override
+		protected void onPostExecute(List<ReadResponse> responses) {
+			StringBuffer sb = new StringBuffer();
+			for (ReadResponse response : responses) {
+				for (Map<String, Object> restaurant : response.getData()) {
+				String name = (String) restaurant.get("name");
+				String address = (String) restaurant.get("address");
+				String phone = (String) restaurant.get("tel");
+				Number distance = (Number) restaurant.get("$distance");
+				sb.append(distance + " meters away: "+name+" @ " +address + ", call "+phone);
+				sb.append(System.getProperty("line.separator"));
+				}  
+			}
+			resultText.setText(sb.toString());
+		}
+
+	} 
 
 }

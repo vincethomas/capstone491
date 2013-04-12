650fc77858e75d4337d2737c7e0b04fec24b44e7
Dylan Bussone <dylanbussone@gmail.com>
2013-04-11 20:31:49 -0700

    camera working

--- src/com/example/allergyapp/MainActivity.java
+++ src/com/example/allergyapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.allergyapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,9 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Button scanButton = (Button) findViewById(R.id.scanButton);
+		scanButton.setOnClickListener(scanDatShit);
+		IntentIntegrator.initiateScan(this);
 	}
 
 	@Override
@@ -17,6 +23,33 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+	
+
+	View.OnClickListener scanDatShit = new View.OnClickListener(){
+        public void onClick(View v) {
+        	
+        	//doesn't work, needs "Activity"
+//        	IntentIntegrator.initiateScan(this);
+        }
+    };
+	
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    switch(requestCode) {
+	        case IntentIntegrator.REQUEST_CODE: {
+	            if (resultCode != RESULT_CANCELED) {
+	                IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+	                if (scanResult != null) {
+	                    String upc = scanResult.getContents();
+	                    
+	                    //put whatever you want to do with the code here
+	                    System.out.println("Success: " + upc);
+	                    
+	                }
+	            }
+	            break;
+	        }
+	    }
 	}
 
 }
